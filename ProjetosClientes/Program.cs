044cd042acd4494d3250416b551654d5504a0865
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-05-14 20:09:19 -0300

    Criando Uma Conexão

--- ProjetosClientes/Program.cs
+++ ProjetosClientes/Program.cs
@@ -6,6 +6,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(
+                "User Id=sa;Password=12345;" +
+                "Server=localhost\\SQLEXPRESS;" +
+                "Database=projetoclientes;" +
+                "Trusted_Connection=False;"
+                );
+
+            using (SqlConnection conexao = new SqlConnection(builder.ConnectionString))
+            {
+                string sql = "SELECT * FROM clientes;";
+
+                using (SqlCommand comando = new SqlCommand(sql, conexao))
+                {
+                    conexao.Open();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error:"+e.ToString());
+        }
     }
 }
