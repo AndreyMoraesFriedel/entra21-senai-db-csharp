408b4d62550308d8a8437fd06f86fd92fa26996f
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-05-14 22:04:15 -0300

    Adicionando e Mostrando Listas

--- ProjetoListas/Program.cs
+++ ProjetoListas/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ClientesPF cl1 = new ClientesPF();
+        cl1.id = 1;
+        cl1.name = "Andrey";
+
+        List <ClientesPF> lista_clientes = new List<ClientesPF> ();
+
+        lista_clientes.Add(new ClientesPF());
+        lista_clientes.Add (cl1);
+        lista_clientes.Add(new ClientesPF() { id = 3, name = "Bruno" });
+
+        foreach(ClientesPF aux in lista_clientes)
+        {
+            Console.WriteLine("Cliente: {0} ", aux.name);
+        }
     }
 }
